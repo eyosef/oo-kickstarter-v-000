7f3d3d8d29216ea4feafc4d0263cb5161d345fb4
Eden Mesfun <emesfun@gmail.com>
2018-02-14 20:17:45 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,7 +12,7 @@ class Backer
   def back_project(project)
     @backed_projects << project
 
-    if project
+    if project == self.project
 
     #project.each{|project_name| Project.new(project_name)}
     #Project.backer = self.name #FIX
