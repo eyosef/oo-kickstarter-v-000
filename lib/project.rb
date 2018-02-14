99307a89b2e04be2153291ab09935de5dc65b2f9
Eden Mesfun <emesfun@gmail.com>
2018-02-14 20:43:21 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -3,13 +3,13 @@ class Project
   attr_accessor :backers, :title, :backer
 
   def initialize(title)
-    @@backers = []
+    @backers = []
     @title = title
     @backer = backer
   end
 
   def self.add_backer(backer)
-    @@backers << backer
+    @backers << backer
   end
 
 end
